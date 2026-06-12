717e20af9e76ec325fc4066b516059b3f49c410d
didnei <jsh329933@gmail.com>
2026-06-12 17:28:09 +0900

    Add files via upload

---
+++ api/supabase-config.js
@@ -0,0 +1,22 @@
+export default function handler(req, res) {
+  // Enable CORS
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
+  // Retrieve Vercel Environment Variables
+  const url = process.env.SUPABASE_URL || '';
+  const key = process.env.SUPABASE_ANON_KEY || '';
+
+  // Return configurations as JSON
+  res.status(200).json({ url, key });
+}
